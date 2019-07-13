0d6d2c5fe2411fc4ec3e2036b533dbb0d3a58576
Sean Davis <sean.dev.davis@gmail.com>
2019-07-13 10:29:16 -0500

    minor changes

--- src/javascripts/helpers/projectData/projects.js
+++ src/javascripts/helpers/projectData/projects.js
@@ -7,13 +7,13 @@ import work from './projectData';
 const projectStringBuilder = () => {
   work.getProjects().then((projects) => {
     let domString = '';
-    domString += '<div class="row">';
+    // domString += '<div class="row">';
     projects.forEach((project) => {
-      domString += '<div class="col-4">';
-      domString += '<div class="card" style="width: 19rem;">';
+      domString += '<div class="col-lg-4 proCards">';
+      domString += '<div class="card" style="width: 22rem;">';
       domString += `<img src="${project.screenshot}" class="card-img-top proj-image" alt="Project Image">`;
       domString += '<div class="card-body text-center proj-body">';
-      domString += `<h5 class="card-title">${project.title}</h5>`;
+      domString += `<h5 class="card-title c-header">${project.title}</h5>`;
       domString += `<p class="card-text description">${project.description}</p>`;
       domString += `<button class="web"><a href="${project.url}">Website</a></button>`;
       domString += `<button class="git"><a href="${project.githubUrl}">Github</a></button>`;
@@ -21,7 +21,7 @@ const projectStringBuilder = () => {
       domString += '</div>';
       domString += '</div>';
     });
-    domString += '</div>';
+    // domString += '</div>';
     util.printToDom('exercises', domString);
   })
     .catch(err => console.error('Could not get projects', err));
